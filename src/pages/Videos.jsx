5694f899363408d7ca88afcfcc8b8e77e3230016
Jeong Ye Bin <dpqls724@gmail.com>
2023-04-20 16:22:02 +0900

    youtube page 작업

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
+import axios from "axios";
+
+const Videos = () => {
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return axios
+      .get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => {
+        console.log(res);
+        return res.data.items;
+      });
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>;
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong😖</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Videos;
