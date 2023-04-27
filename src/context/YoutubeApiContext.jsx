a9b8bc0c84082c5f77ceea59bdef9a08121412b1
Jeong Ye Bin <dpqls724@gmail.com>
2023-04-27 14:26:50 +0900

    youtube clone coding

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext } from "react";
-import FakeYoutubeClient from "../api/fakeYoutubeClient";
-// import YoutubeClient from "../api/youtubeClient";
+// import FakeYoutubeClient from "../api/fakeYoutubeClient";
+import YoutubeClient from "../api/youtubeClient";
 import Youtube from "../api/youtube";
 
 export const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient();
-// const client = new YoutubeClient();
+// const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
